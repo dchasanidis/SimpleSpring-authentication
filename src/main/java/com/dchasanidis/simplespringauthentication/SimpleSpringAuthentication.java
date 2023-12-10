96bdfb19bb300995ed6cd53e0477665ee5871b60
Dimitris Chasanidis <dchasanidis@dol-guldur.speedport.ip>
2023-12-10 10:46:43 +0100

    rename main

--- src/main/java/com/dchasanidis/simplespringauthentication/SimpleSpringAuthentication.java
+++ src/main/java/com/dchasanidis/simplespringauthentication/SimpleSpringAuthentication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChasBankAuthenticationApplication {
+public class SimpleSpringAuthentication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChasBankAuthenticationApplication.class, args);
+        SpringApplication.run(SimpleSpringAuthentication.class, args);
     }
 
 }

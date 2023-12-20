9453346358cdce6a527c8197335ca0ae2ffe0f2b
dchasanidis <dimitrischasanidis@gmail.com>
2023-12-20 12:58:12 +0100

    Add active profile for integration test

--- src/test/java/com/dchasanidis/simplespringauthentication/SimpleSpringAuthenticationTests.java
+++ src/test/java/com/dchasanidis/simplespringauthentication/SimpleSpringAuthenticationTests.java
@@ -1,13 +1,21 @@
 package com.dchasanidis.simplespringauthentication;
 
+import com.dchasanidis.simplespringauthentication.model.entities.UserEntity;
+import com.dchasanidis.simplespringauthentication.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SimpleSpringAuthenticationTests {
+    @Autowired
+    UserRepository userRepository;
 
     @Test
     void contextLoads() {
+        userRepository.save(new UserEntity().setEmail("email").setPassword("password").setUsername("username"));
     }
 
 }

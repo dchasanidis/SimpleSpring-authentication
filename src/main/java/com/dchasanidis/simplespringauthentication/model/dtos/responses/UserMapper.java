b0561f154bcbab7283ed94b290c5d0aa72862680
dchasanidis <dimitrischasanidis@gmail.com>
2024-02-11 20:03:29 +0200

    make mapper use generated dto
    Signed-off-by: dchasanidis <dimitrischasanidis@gmail.com>

--- src/main/java/com/dchasanidis/simplespringauthentication/model/dtos/responses/UserMapper.java
+++ src/main/java/com/dchasanidis/simplespringauthentication/model/dtos/responses/UserMapper.java
@@ -1,10 +1,11 @@
 package com.dchasanidis.simplespringauthentication.model.dtos.responses;
 
+import com.dchasanidis.simplespringauthentication.model.User;
 import com.dchasanidis.simplespringauthentication.model.entities.UserEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserMapper {
-    UserDto toDto(final UserEntity userEntity);
+    User toDto(final UserEntity userEntity);
 }
